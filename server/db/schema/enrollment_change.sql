51cc2cff91d983a21bf41922ffc58d7bf11829b1
Angelina Wang <wangangeliina@gmail.com>
2025-11-27 21:34:56 -0800

    "GCF-11 Create Enrollment Change & Media Change Tables"

---
+++ server/db/schema/enrollment_change.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS enrollment_change (
+    id BIGSERIAL PRIMARY KEY,
+    update_id BIGINT NOT NULL,
+    enrollment_change SMALLINT NOT NULL,
+    graduated_change SMALLINT NOT NULL,
+
+    CONSTRAINT fk_program_update
+        FOREIGN KEY (update_id)
+            REFERENCES program_update(id)
+            ON DELETE CASCADE
+);
