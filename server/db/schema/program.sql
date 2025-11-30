0f508a82b3f894b28fc22f6fbae0d41e72ca7530
Sophie Choi <103692454+sophieelissachoi@users.noreply.github.com>
2025-11-30 01:26:44 -0800

    fix: deleted the dummy tables

--- server/db/schema/program.sql
+++ server/db/schema/program.sql
@@ -1,31 +1,19 @@
 CREATE TYPE status_type AS ENUM ('Active', 'Inactive');
 
-CREATE TABLE dummy_user (
-    id BIGSERIAL PRIMARY KEY UNIQUE NOT NULL
-);
-
-CREATE TABLE dummy_country (
-    id SERIAL PRIMARY KEY UNIQUE NOT NULL
-);
-
-CREATE TABLE dummy_partner_organization (
-    id BIGSERIAL PRIMARY KEY NOT NULL
-);
-
 CREATE TABLE program (
     id BIGSERIAL PRIMARY KEY UNIQUE NOT NULL,
     created_by BIGINT NOT NULL,
-    CONSTRAINT fk_created_by FOREIGN KEY (created_by) REFERENCES dummy_user(id),
+    CONSTRAINT fk_created_by FOREIGN KEY (created_by) REFERENCES user(id),
     name VARCHAR(70) NOT NULL,
     date_created TIMESTAMP NOT NULL,
     country INTEGER NOT NULL,
-    CONSTRAINT fk_country FOREIGN KEY (country) REFERENCES dummy_country(id),
+    CONSTRAINT fk_country FOREIGN KEY (country) REFERENCES country(id),
     title VARCHAR(140) NOT NULL,
     description VARCHAR,
     primary_language VARCHAR,
     playlist_link VARCHAR,
     partner_org BIGINT NOT NULL,
-    CONSTRAINT fk_partner_org FOREIGN KEY (partner_org) REFERENCES dummy_partner_organization(id),
+    CONSTRAINT fk_partner_org FOREIGN KEY (partner_org) REFERENCES partner_organization(id),
     status status_type NOT NULL,
     launch_date DATE NOT NULL
 );
