02b38ccaac9ddc1f782ec0736683c2ddfe96117c
Daiki Narimoto <daiki.narimoto@gmail.com>
2026-01-06 17:53:09 +0900

    Created backend API routes for region and country tables.

---
+++ server/routes/country.js
@@ -0,0 +1,100 @@
+// for reference only
+import { keysToCamel } from "@/common/utils";
+import express from "express";
+import { db } from "../db/db-pgp";
+
+const countryRouter = express.Router();
+countryRouter.use(express.json());
+
+countryRouter.get("/", async (req, res) => {
+  try {
+    // Query database
+    const data = await db.query(`SELECT * FROM country`);
+    res.status(200).json(keysToCamel(data));
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+countryRouter.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    // rename entry to something relevant to the route
+    const country = await db.query(
+      `SELECT ALL * FROM country WHERE id = $1`,
+      [id]
+    );
+
+    if (country.length === 0){
+      return res.status(404).send("Item not found");
+    }
+
+    res.status(200).json(keysToCamel(country[0]));
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+countryRouter.post("/", async (req, res) => {
+  try {
+    const { id, region_id, name, last_modified } = req.body
+    const newCountry = await db.query(
+      `INSERT INTO country (id, region_id, name, last_modified) 
+      VALUES ($1, $2, $3, $4) 
+      RETURNING *`,
+      [id, region_id, name, last_modified]
+    );
+    res.status(201).json(keysToCamel(newCountry[0]));
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+countryRouter.put("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { region_id, name, last_modified  } = req.body;
+    const updatedCountry = await db.query(
+      `UPDATE country SET
+        region_id = COALESCE($1, region_id),
+        name = COALESCE($2, name),
+        last_modified = COALESCE($3, last_modified)
+        WHERE id = $4
+        RETURNING *;`,
+      [region_id, name, last_modified, id]
+    );
+
+    if (updatedCountry.length === 0){
+      return res.status(404).send("Item not found");
+    }
+
+    res.status(200).json(keysToCamel(updatedCountry[0]));
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+countryRouter.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedCountry = await db.query(
+      `DELETE FROM country WHERE id = $1 RETURNING *`,
+      [id]
+    );
+
+    if (deletedCountry.length === 0){
+      return res.status(404).send("Item not found");
+    }
+
+    res.status(200).json(keysToCamel(deletedCountry[0]));
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+export { countryRouter };
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,15 +1,16 @@
 import { verifyToken } from "@/middleware";
-import { sampleRouter } from "@/routes/sample"; // TODO: delete sample router
 import { usersRouter } from "@/routes/users";
+import { countryRouter } from "@/routes/country";
+import {regionRouter} from "@/routes/region";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
-import schedule from "node-schedule"; // TODO: Keep only if scheduling cronjobs
+// import schedule from "node-schedule"; // TODO: Keep only if scheduling cronjobs
 
 dotenv.config();
 
-schedule.scheduleJob("0 0 0 0 0", () => console.info("Hello Cron Job!")); // TODO: delete sample cronjob
+// schedule.scheduleJob("0 0 0 0 0", () => console.info("Hello Cron Job!")); // TODO: delete sample cronjob
 
 const CLIENT_HOSTNAME =
   process.env.NODE_ENV === "development"
@@ -30,8 +31,10 @@ if (process.env.NODE_ENV === "production") {
   app.use(verifyToken);
 }
 
-app.use("/", sampleRouter); // TODO: delete sample endpoint
+// app.use("/", sampleRouter); // TODO: delete sample endpoint
 app.use("/users", usersRouter);
+app.use("/country", countryRouter);
+app.use("/region", regionRouter);
 
 // Listening is moved to server.ts to enable importing app in tests
 export default app;
