47dc4d4efdf260da85d5b21208f0c49c8695eaf5
Aditi Varia <aditivaria@gmail.com>
2026-01-10 19:09:58 -0800

    GCF26-Create-Enrollment-Change-And-Media-Change-API-Endpoints

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,15 +1,17 @@
 import { verifyToken } from "@/middleware";
-import { sampleRouter } from "@/routes/sample"; // TODO: delete sample router
+//import { sampleRouter } from "@/routes/sample"; // TODO: delete sample router
 import { usersRouter } from "@/routes/users";
+import { enrollmentChangeRouter } from "@/routes/enrollmentChange";
+import { mediaChangeRouter } from "@/routes/mediaChange";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
-import schedule from "node-schedule"; // TODO: Keep only if scheduling cronjobs
+//import schedule from "node-schedule"; // TODO: Keep only if scheduling cronjobs
 
 dotenv.config();
 
-schedule.scheduleJob("0 0 0 0 0", () => console.info("Hello Cron Job!")); // TODO: delete sample cronjob
+//schedule.scheduleJob("0 0 0 0 0", () => console.info("Hello Cron Job!")); // TODO: delete sample cronjob
 
 const CLIENT_HOSTNAME =
   process.env.NODE_ENV === "development"
@@ -30,8 +32,10 @@ if (process.env.NODE_ENV === "production") {
   app.use(verifyToken);
 }
 
-app.use("/", sampleRouter); // TODO: delete sample endpoint
+//app.use("/", sampleRouter); // TODO: delete sample endpoint
 app.use("/users", usersRouter);
+app.use("/enrollmentChange", enrollmentChangeRouter);
+app.use("/mediaChange", mediaChangeRouter);
 
 // Listening is moved to server.ts to enable importing app in tests
 export default app;
