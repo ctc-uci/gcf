0c7cd915150e5b137396e3d4e4f5ffd4fac73183
Sophie Choi <103692454+sophieelissachoi@users.noreply.github.com>
2026-01-23 21:41:57 -0800

    feat: added switch statements to show/hide components based on user's role

--- client/src/components/updates/ProgramUpdatesTable.jsx
+++ client/src/components/updates/ProgramUpdatesTable.jsx
@@ -1,3 +1,83 @@
-export const ProgramUpdatesTable = () => {
-  return <></>;
+import { DownloadIcon, HamburgerIcon, SearchIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Flex,
+  Heading,
+  Input,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
+
+export const ProgramUpdatesTable = ({ programUpdatesData, gcfUserData }) => {
+  const usersById = Object.fromEntries(gcfUserData.map((u) => [u.id, u]));
+  return (
+    <>
+      <Box
+        mt="30px"
+        ml="10px"
+      >
+        <Flex
+          gap={10}
+          mb="20px"
+        >
+          <Heading> Updates</Heading>
+          <SearchIcon
+            mt="10px"
+            ml="10px"
+          />
+          <Input
+            placeholder="Type to search"
+            variant="flushed"
+            w="200px"
+          />
+          <HamburgerIcon mt="10px" />
+          <DownloadIcon mt="10px" />
+        </Flex>
+
+        <TableContainer>
+          <Table variant="simple">
+            <Thead>
+              <Tr>
+                <Th>Time</Th>
+                <Th>Notes</Th>
+                <Th>Program</Th>
+                <Th>Author</Th>
+                <Th>Status</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {programUpdatesData.map((row) => {
+                return (
+                  <Tr key={row.updateId}>
+                    <Td>{row.updateDate}</Td>
+                    <Td>{row.note}</Td>
+                    <Td>{programsById[row.programId]?.name}</Td>
+                    <Td>
+                      {usersById[row.createdBy]?.firstName}{" "}
+                      {usersById[row.createdBy]?.lastName}
+                    </Td>
+                    <Box
+                      borderRadius="full"
+                      bg="gray.200"
+                      w="100px"
+                      h="40px"
+                      alignItems="center"
+                      pb="50px"
+                    >
+                      <Td> {programsById[programId]?.status} </Td>
+                    </Box>
+                  </Tr>
+                );
+              })}
+            </Tbody>
+          </Table>
+        </TableContainer>
+      </Box>
+    </>
+  );
 };
--- client/src/components/updates/UpdatesPage.jsx
+++ client/src/components/updates/UpdatesPage.jsx
@@ -1,35 +1,52 @@
 import { useEffect, useState } from "react";
 
+import { getAuth } from "firebase/auth";
+
 import { MediaUpdatesTable } from "./MediaUpdatesTable";
 import { ProgramAccountUpdatesTable } from "./ProgramAccountUpdatesTable";
-
-// import { ProgramUpdatesTable } from "./ProgramUpdatesTable";
+import { ProgramUpdatesTable } from "./ProgramUpdatesTable";
 
 export const UpdatesPage = () => {
   const [programUpdatesData, setProgramUpdatesData] = useState([]);
   const [mediaUpdatesData, setMediaUpdatesData] = useState([]);
   const [programData, setProgramData] = useState([]);
   const [gcfUserData, setGcfUserData] = useState([]);
+  const [countryData, setCountryData] = useState([]);
+  const [regionData, setRegionData] = useState([]);
+  const [regionalDirectorsData, setRegionalDirectorsData] = useState([]);
 
   useEffect(() => {
     const loadData = async () => {
       try {
-        const [programUpdatesRes, mediaUpdatesRes, programRes, usersRes] =
-          await Promise.all([
-            fetch("http://localhost:3001/program-updates").then((res) =>
-              res.json()
-            ),
-            fetch("http://localhost:3001/mediaChange").then((res) =>
-              res.json()
-            ),
-            fetch("http://localhost:3001/program").then((res) => res.json()),
-            fetch("http://localhost:3001/gcf-users").then((res) => res.json()),
-          ]);
+        const [
+          programUpdatesRes,
+          mediaUpdatesRes,
+          programRes,
+          usersRes,
+          countryRes,
+          regionRes,
+          regionalDirectorRes,
+        ] = await Promise.all([
+          fetch("http://localhost:3001/program-updates").then((res) =>
+            res.json()
+          ),
+          fetch("http://localhost:3001/mediaChange").then((res) => res.json()),
+          fetch("http://localhost:3001/program").then((res) => res.json()),
+          fetch("http://localhost:3001/gcf-users").then((res) => res.json()),
+          fetch("http://localhost:3001/country").then((res) => res.json()),
+          fetch("http://localhost:3001/region").then((res) => res.json()),
+          fetch("http://localhost:3001/regional-directors").then((res) =>
+            res.json()
+          ),
+        ]);
 
         setProgramUpdatesData(programUpdatesRes);
         setMediaUpdatesData(mediaUpdatesRes);
         setProgramData(programRes);
         setGcfUserData(usersRes);
+        setCountryData(countryRes);
+        setRegionData(regionRes);
+        setRegionalDirectorsData(regionalDirectorRes);
       } catch (error) {
         console.error("Fetch error:", error);
       }
@@ -38,19 +55,66 @@ export const UpdatesPage = () => {
     loadData();
   }, []);
 
+  const auth = getAuth();
+  const currentUser = auth.currentUser;
+
+  let visibleMediaUpdates = [];
+  let visibleProgramUpdates = [];
+
+  // const usersById = Object.fromEntries(gcfUserData.map((u) => [u.id, u]));
+  const programUpdatesById = Object.fromEntries(
+    programUpdatesData.map((p) => [p.id, p])
+  );
+  const programsById = Object.fromEntries(programData.map((p) => [p.id, p]));
+  const countriesById = Object.fromEntries(countryData.map((p) => [p.id, p]));
+  const regionsById = Object.fromEntries(regionData.map((p) => [p.id, p]));
+  const regionalDirectorsById = Object.fromEntries(
+    regionalDirectorsData.map((p) => [p.id, p])
+  );
+
+  switch (user?.role) {
+    case "Admin":
+      visibleMediaUpdates = mediaUpdatesData;
+      visibleProgramUpdates = programUpdatesData;
+      break;
+    case "Regional Director":
+      visibleProgramUpdates = programUpdatesData.filter(
+        (row) =>
+          regionsById[countriesById[programsById[row.programId].country].id] ===
+          regionalDirectorsById[currentUser.id].regionId
+      );
+      visibleMediaUpdates = mediaUpdatesData.filter(
+        (row) =>
+          regionsById[
+            countriesById[
+              programsById[programUpdatesById[row.updateId].programId].country
+            ].id
+          ] === regionalDirectorsById[currentUser.id].regionId
+      );
+      break;
+    default: // Project Director
+      visibleProgramUpdates = programUpdatesData.filter(
+        (row) => row.createdBy === currentUser.id
+      );
+  }
+
   return (
     <>
       <MediaUpdatesTable
-        mediaUpdatesData={mediaUpdatesData}
-        programUpdatesData={programUpdatesData}
+        mediaUpdatesData={visibleMediaUpdates}
+        programUpdatesData={visibleProgramUpdates}
         programData={programData}
         gcfUserData={gcfUserData}
-      ></MediaUpdatesTable>
+      />
       <ProgramAccountUpdatesTable
-        programData={programUpdatesData}
+        programData={visibleProgramUpdates}
         gcfUserData={gcfUserData}
         program={programData}
-      ></ProgramAccountUpdatesTable>
+      />
+      <ProgramUpdatesTable
+        programUpdatesData={visibleProgramUpdates}
+        gcfUserData={gcfUserData}
+      />
     </>
   );
 };
