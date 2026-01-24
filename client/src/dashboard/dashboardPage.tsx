f743681f9ac75a0aa85cfa1e59d615ba70614ea0
DerrickThrower <throwerderrick24@gmail.com>
2026-01-23 17:31:03 -0800

    dashboard page update

--- client/src/dashboard/dashboardPage.tsx
+++ client/src/dashboard/dashboardPage.tsx
@@ -1,15 +1,25 @@
-import AdminProgramTable from '@/dashboard/AdminProgramTable'
-import SearchBar from '@/dashboard/SearchBar'
-function DashboardPage() {
-    return (
-        <>
-        <h2>Stats component</h2>
-        
-        <AdminProgramTable/>
+import { Flex, Box, Text, Heading } from "@chakra-ui/react";
+import StatisticsSummary from "./StatisticsSummary";
 
-        </>
-        
-    );
-}
+const DashboardPage = () => {
+  return (
+    <Flex
+      direction="column"
+      align="center"
+      minH="100vh"
+      gap={6}
+      as="main"
+    >
+      <StatisticsSummary />
+
+      <Box as="section" p={4} textAlign="center">
+        <Heading as="h2" size="md" mb={2}>
+          Program Table
+        </Heading>
+        <Text>This is the program table component</Text>
+      </Box>
+    </Flex>
+  );
+};
 
 export default DashboardPage;
