92fe468eb1837ea9050ac1d51272a174229e9a3b
Gina <fluffylennys@gmail.com>
2026-01-24 17:32:22 -0800

    Added tags to make create program form match lo-fi

--- client/src/components/updates/ProgramUpdateForm.jsx
+++ client/src/components/updates/ProgramUpdateForm.jsx
@@ -1,4 +1,9 @@
 
+import { useState } from 'react';
+import axios from 'axios';
+
+import { ArrowRightIcon } from '@chakra-ui/icons'
+
 import 
 { VStack,
   Heading,
@@ -11,66 +16,77 @@ import
   Select,
   Box,
   Button,
-  Textarea
+  Textarea,
 } from '@chakra-ui/react'
 
 export const ProgramUpdateForm = () => {
+    // const [formData, setFormData] = useState({
+    //     date: '',
+    //     studentsEnrolled: 0,
+    //     instrument: '',
+    //     notes: '',
+    //     media: []
+    // });
+    
     return (
-        <VStack>
-            <Heading>
-                Create New Update
-            </Heading>
+        <VStack p={8} width='35%' borderWidth="1px" align="start" spacing={4}>
+            <HStack>
+                <ArrowRightIcon color="gray"></ArrowRightIcon>
+                <Heading size="md">
+                    Create New Update
+                </Heading>
+            </HStack>
             <FormControl>
-                <FormLabel>
+                <FormLabel fontWeight="normal" color="gray">
                     Date
                 </FormLabel>
-                <Input placeholder="MM/DD/YYYY"/>
+                <Input placeholder="MM/DD/YYYY" bg="gray.100"/>
             </FormControl>
             <FormControl>
-                <FormLabel>
+                <FormLabel fontWeight="normal" color="gray">
                     # Students currently enrolled
                 </FormLabel>
                 <NumberInput>
-                    <NumberInputField></NumberInputField>
+                    <NumberInputField bg="gray.100"></NumberInputField>
                 </NumberInput>
             </FormControl>
             <FormControl>
                 <HStack>
-                    <Box>
-                        <FormLabel>
+                    <Box width="40%">
+                        <FormLabel fontWeight="normal" color="gray">
                             Instrument Type
                         </FormLabel>
-                        <Select placeholder="Ukelele">
+                        <Select placeholder="Ukelele" bg="gray.100">
 
                         </Select>
                     </Box>
-                    <Box>
-                        <FormLabel>
+                    <Box width="20%">
+                        <FormLabel fontWeight="normal" color="gray">
                             # Donated
                         </FormLabel>
-                        <NumberInput>
+                        <NumberInput bg="gray.100">
                             <NumberInputField></NumberInputField>
                         </NumberInput>
                     </Box>
                 </HStack>
             </FormControl>
             <FormControl>
-                <Button>
+                <Button borderWidth={1} borderColor="black" bg="white">
                     + Add Instrument
                 </Button>
             </FormControl>
             <FormControl>
-                <FormLabel>
+                <FormLabel fontWeight="normal" color="gray">
                     Notes
                 </FormLabel>
-                <Textarea></Textarea>
+                <Textarea borderWidth={1} borderColor="black" bg="gray.100"></Textarea>
             </FormControl>
             <FormControl>
-                <Button>
+                <Button borderWidth={1} borderColor="black" bg="white">
                     + Add Media
                 </Button>
             </FormControl>
-            <Button>
+            <Button width="35%" alignSelf="center" borderWidth={1} borderColor="black" bg="gray.400">
                 Submit
             </Button>
         </VStack>
