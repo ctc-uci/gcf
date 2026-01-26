21fff5f229c0d138f9628c96990b4e8a2b38f7ea
Gina <fluffylennys@gmail.com>
2026-01-26 00:52:29 -0800

    Implemented POST requests for enrollment change, instrument change, instruments, and program updates (fat commit my condolences to whoever is reviewing this i am so sorry)

--- client/src/components/updates/ProgramUpdateForm.jsx
+++ client/src/components/updates/ProgramUpdateForm.jsx
@@ -19,49 +19,44 @@ import
   Button,
   Box,
   Textarea,
-  SlideFade,
-  useDisclosure,
+//   useDisclosure,
   Tag,
   TagLabel,
   TagCloseButton
 } from '@chakra-ui/react'
 
 export const ProgramUpdateForm = () => {
-    const { isOpen, onToggle } = useDisclosure()
-    const [newInstrumentName, setNewInstrumentName] = useState('')
-    const [instruments, setInstruments] = useState([])
+    // const { isOpen, onToggle } = useDisclosure()
+    const [isAddingInstrument, setIsAddingInstrument] = useState(false)
+    
+    const [title, setTitle] = useState('')
+    const [date, setDate] = useState('')
+    const [enrollmentNumber, setEnrollmentNumber] = useState(null)
+    const [graduatedNumber, setGraduatedNumber] = useState(null)
+    const [notes, setNotes] = useState('')
+    
     const [selectedInstrument, setSelectedInstrument] = useState('')
+    const [newInstrumentName, setNewInstrumentName] = useState('')
     const [quantity, setQuantity] = useState(0)
+    
+    const [existingInstruments, setExistingInstruments] = useState([])
     const [addedInstruments, setAddedInstruments] = useState({})
+    const [newInstruments, setNewInstruments] = useState([])
 
-    // fetch instruments when component loads
+    // fetch instruments when form loads
     useEffect(() => {
         const fetchInstruments = async () => {
             try {
                 const response = await axios.get('http://localhost:3001/instruments');
-                setInstruments(response.data);
+                setExistingInstruments(response.data);
             } catch (error) {
                 console.error('Error fetching instruments:', error);
-                // continue rendering even if fetch fails
-                setInstruments([]);
+                // keep rendering if fetch fails
+                setExistingInstruments([]);
             }
         };
         fetchInstruments();
     }, []);
-
-    const handleConfirm = () => {
-        if (!selectedInstrument || quantity === 0) {
-            return;
-        }
-                
-        setAddedInstruments(prev => ({
-            ...prev,
-            [selectedInstrument]: parseInt(quantity)
-        }));
-        
-        setSelectedInstrument('');
-        setQuantity(0);
-    };
 
     const removeInstrument = (name) => {
         setAddedInstruments(prev => {
@@ -70,110 +65,251 @@ export const ProgramUpdateForm = () => {
             return updated;
         });
     };
-    
-    const addInstrument = async () => {
-        if (!newInstrumentName.trim()) {
-            return;
-        }
-        // might want to check if instrument alr exists before adding
-        try {
-            await axios.post('http://localhost:3001/instruments', {
-                name: newInstrumentName
-            });
 
-            // refetch instruments after adding to update dropdown menu
-            const response = await axios.get('http://localhost:3001/instruments');
-            setInstruments(response.data);
+    const validNewInstrument = () => {
+        return newInstrumentName.trim().length > 0 && 
+            !existingInstruments.some(instr => instr.name.toLowerCase() === newInstrumentName.trim().toLowerCase());
+    }
 
+    const handleNewInstrument = () => {
+        if (!validNewInstrument()) {
             setNewInstrumentName('');
-            onToggle();
-        } catch (error) {
-            console.error('Error adding instrument:', error);
+            return;
         }
     };
 
+    const handleConfirmAddInstrument = () => {
+
+        handleNewInstrument();
+
+        if ((!selectedInstrument && !newInstrumentName)) {
+            console.log("nothing selected")
+            return; 
+        }
+        else if (quantity === 0) {
+            console.log("quantity 0")
+            return;
+        }
+        else if ((selectedInstrument && newInstrumentName)) {
+            console.log("both selected")
+            return; 
+        }
+
+        if (newInstrumentName) {
+            setNewInstruments(prev => [...prev, newInstrumentName]);
+        }
+               
+        setAddedInstruments(prev => ({
+            ...prev,
+            [selectedInstrument || newInstrumentName]: parseInt(quantity)
+        }));
+        
+        
+        setNewInstrumentName('');
+        setSelectedInstrument('');
+        setQuantity(0);
+    };
+
+    const handleSubmit = async () => {
+        try {
+            // validate required fields
+            if (!title.trim() || !date || !notes.trim()) {
+                console.error('Missing required fields.');
+            }
+            
+            // create program update
+            const programUpdateData = {
+                title: title.trim(),
+                program_id: 26, // TODO: replace with actual program id
+                created_by: 1, // TODO: replace with actual user id
+                update_date: date,
+                note: notes.trim()
+            };
+            
+            const response = await axios.post('http://localhost:3001/program-updates', programUpdateData);
+            const programUpdateId = response.data.id;
+
+            // add new instruments to DB
+            console.log('New instruments to add:', newInstruments);
+            for (const instrumentName of newInstruments) {
+                try {
+                    await axios.post('http://localhost:3001/instruments', {
+                        name: instrumentName
+                    });
+                    console.log(`Added new instrument: ${instrumentName}`);
+                } catch (error) {
+                    console.error(`Error adding instrument ${instrumentName}:`, error);
+                }
+            }
+
+            // refetch instruments to get their ids
+            const instrumentsResponse = await axios.get('http://localhost:3001/instruments');
+            setExistingInstruments(instrumentsResponse.data);
+
+            console.log('Added Instruments:', addedInstruments);
+            // create instrument changes if any instruments were added
+            if (Object.keys(addedInstruments).length > 0) 
+                {
+                const instrumentChanges = Object.entries(addedInstruments).map(([name, qty]) => {
+                    const instrument = instrumentsResponse.data.find(instr => instr.name === name);
+                    console.log('instrumentsResponse, update id, and qty', instrumentsResponse, programUpdateId, qty);
+                    return {
+                        instrument_id: instrument.id,
+                        update_id: programUpdateId,
+                        amount_changed: qty
+                    };
+                });
+                console.log('Sending instrument changes:', instrumentChanges[0]);
+                
+                for (const change of instrumentChanges) {
+                await axios.post(
+                    'http://localhost:3001/instrument-changes',
+                    {
+                        instrumentId: change.instrument_id,
+                        updateId: change.update_id,
+                        amountChanged: change.amount_changed
+                    }
+                );
+}
+            }
+
+            // create enrollment change entries if both enrollment # and graduated # are provided
+            if (enrollmentNumber !== null && graduatedNumber !== null) {
+                console.log('Sending enrollment change:', { update_id: programUpdateId, enrollment_change: enrollmentNumber, graduated_change: graduatedNumber });
+                await axios.post('http://localhost:3001/enrollmentChange', {
+                    update_id: programUpdateId,
+                    enrollment_change: enrollmentNumber,
+                    graduated_change: graduatedNumber
+                });
+            }
+
+            console.log('Sending program update data:', programUpdateId, enrollmentNumber, graduatedNumber);
+
+            // clear form if submission was successful
+            setTitle('');
+            setDate('');
+            setEnrollmentNumber(null);
+            setGraduatedNumber(null);
+            setNotes('');
+            setAddedInstruments({});
+            setNewInstruments([]);
+
+            console.log('Program update submitted successfully');
+        } catch (error) {
+            console.error('Error submitting program update:', error);
+        }
+    }
+
     return (
-        <VStack p={8} width='35%' borderWidth="1px" align="start" spacing={4}>
+        <VStack 
+            p={8} 
+            width='35%' 
+            borderWidth="1px" 
+            align="start" 
+            spacing={4}
+        >
             <HStack>
                 <ArrowRightIcon color="gray"></ArrowRightIcon>
                 <Heading size="md">
                     Create New Update
                 </Heading>
             </HStack>
-            <FormControl>
+            <FormControl isRequired>
+                <FormLabel fontWeight="normal" color="gray">
+                    Title
+                </FormLabel>
+                <Input 
+                    type="text" 
+                    placeholder="Enter Title Here" 
+                    bg="gray.100" 
+                    value={title} 
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+            </FormControl>
+            <FormControl isRequired>
                 <FormLabel fontWeight="normal" color="gray">
                     Date
                 </FormLabel>
-                <Input type="date" placeholder="MM/DD/YYYY" bg="gray.100"/>
+                <Input 
+                    type="date" 
+                    placeholder="MM/DD/YYYY" 
+                    bg="gray.100" 
+                    value={date} 
+                    onChange={(e) => setDate(e.target.value)}
+                />
             </FormControl>
+            <HStack>
             <FormControl>
                 <FormLabel fontWeight="normal" color="gray">
                     # Students currently enrolled
                 </FormLabel>
-                <NumberInput width="30%">
+                <NumberInput 
+                    width="30%" 
+                    value={enrollmentNumber || ''} 
+                    onChange={(value) => setEnrollmentNumber(value ? parseInt(value) : null)}
+                >
                     <NumberInputField bg="gray.100"></NumberInputField>
                 </NumberInput>
             </FormControl>
-            <HStack width="100%">
-                <Box>
+            <FormControl>
+                <FormLabel fontWeight="normal" color="gray">
+                    # Students graduated
+                </FormLabel>
+                <NumberInput 
+                    width="30%" 
+                    value={graduatedNumber || ''} 
+                    onChange={(value) => setGraduatedNumber(value ? parseInt(value) : null)}
+                >
+                    <NumberInputField bg="gray.100"></NumberInputField>
+                </NumberInput>
+            </FormControl>
+            </HStack>
+            <FormControl>
+                <FormLabel>Instruments & Quantity</FormLabel>
+                {!isAddingInstrument && 
+                <Button onClick={() => setIsAddingInstrument(true)}>+ Add</Button>}
+                {isAddingInstrument && 
+                <HStack>
+                    <Select 
+                        placeholder="Select" 
+                        bg="gray.100"
+                        value={selectedInstrument}
+                        onChange={(e) => setSelectedInstrument(e.target.value)}
+                    >
+                        {existingInstruments.map(instrument => (
+                            <option key={instrument.id} value={instrument.name}>
+                                {instrument.name}
+                            </option>
+                        ))}
+                    </Select>
                     <FormControl>
-                        <FormLabel fontWeight="normal" color="gray">
-                            Instruments
-                        </FormLabel>
-                        <Select 
-                            placeholder="Select Instrument" 
-                            bg="gray.100"
-                            value={selectedInstrument}
-                            onChange={(e) => setSelectedInstrument(e.target.value)}
-                        >
-                            {instruments.map(instrument => (
-                                <option key={instrument.id} value={instrument.name}>
-                                    {instrument.name}
-                                </option>
-                            ))}
-                        </Select>
+                    <Input 
+                        placeholder="New Instrument" 
+                        bg="gray.100"
+                        value={newInstrumentName}
+                        onChange={(e) => setNewInstrumentName(e.target.value)} 
+                    />
                     </FormControl>
-                </Box>
-                <Box>
-                    <FormLabel fontWeight="normal" color="gray">
-                            Quantity
-                    </FormLabel>
-                    <NumberInput 
-                        maxW="80px"
+                    <NumberInput
                         value={quantity}
                         onChange={(value) => setQuantity(parseInt(value) || 0)}
                     >
-                        <NumberInputField />
+                        <NumberInputField/>
                         <NumberInputStepper>
                             <NumberIncrementStepper/>
                             <NumberDecrementStepper/>
                         </NumberInputStepper>
                     </NumberInput>
-                </Box>
-                <Button mt={33} onClick={handleConfirm}>
-                    Confirm
-                </Button>
-            </HStack>
-                <FormControl>
-                    <HStack>
-                        <Button onClick={onToggle} size='sm'borderWidth={1} borderColor="black" bg="white">
-                            + Add Instrument
-                        </Button>
-                        <SlideFade in={isOpen} offsetX="-100px" offsetY="0px">
-                            <HStack>
-                                <Input 
-                                    placeholder="New Instrument" 
-                                    bg="gray.100"
-                                    value={newInstrumentName}
-                                    onChange={(e) => setNewInstrumentName(e.target.value)}
-                                />
-                                <Button onClick={addInstrument}>
-                                    OK
-                                </Button>
-                            </HStack>
-                        </SlideFade>
-                    </HStack>
-                </FormControl>
+                    <Button 
+                        onClick={() => {setIsAddingInstrument(false); 
+                                        handleConfirmAddInstrument();
+                                        }}
+                    >
+                    Add
+                    </Button>
+                </HStack>               
+                }
+            </FormControl>
                 {
                     Object.keys(addedInstruments).length > 0 && (
                         <HStack width="100%" flexWrap="wrap" spacing={2}>
@@ -186,18 +322,35 @@ export const ProgramUpdateForm = () => {
                         </HStack>
                     )
                 }
-            <FormControl>
+            <FormControl isRequired>
                 <FormLabel fontWeight="normal" color="gray">
                     Notes
                 </FormLabel>
-                <Textarea borderWidth={1} borderColor="black" bg="gray.100"></Textarea>
+                <Textarea 
+                    borderWidth={1} 
+                    borderColor="black" 
+                    bg="gray.100" 
+                    value={notes} 
+                    onChange={(e) => setNotes(e.target.value)}
+                >
+                </Textarea>
             </FormControl>
             <FormControl>
-                <Button borderWidth={1} borderColor="black" bg="white">
+                <Button 
+                    borderWidth={1} 
+                    borderColor="black" 
+                    bg="white">
                     + Add Media
                 </Button>
             </FormControl>
-            <Button width="35%" alignSelf="center" borderWidth={1} borderColor="black" bg="gray.400">
+            <Button 
+                width="35%" 
+                alignSelf="center" 
+                borderWidth={1} 
+                borderColor="black" 
+                bg="gray.400"
+                onClick={handleSubmit}
+            >
                 Submit
             </Button>
         </VStack>
