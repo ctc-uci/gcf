7df907356923cfb9b03cd14aeba9338759b0a806
Lawrence Zhou <zhoulh@uci.edu>
2026-01-25 19:35:48 -0800

    Added Autofill for instruments and quantity

--- client/src/components/updates/ProgramUpdateEditForm.jsx
+++ client/src/components/updates/ProgramUpdateEditForm.jsx
@@ -17,7 +17,10 @@ import {
   NumberDecrementStepper,
   Button,
   HStack,
-  Spacer
+  Spacer,
+  Tag,
+  TagLabel,
+  TagCloseButton
 } from "@chakra-ui/react";
 
 import { useState, useEffect } from "react";
@@ -28,13 +31,16 @@ import { useState, useEffect } from "react";
 } from "@chakra-ui/icons";*/
 
 export const ProgramUpdateEditForm = ( {programUpdateId} ) => {
-    console.log(programUpdateId)
-
     const [isAddingInstrument, setIsAddingInstrument] = useState(false)
     const [isAddingLanguage, setIsAddingLanguage] = useState(false)
     const [isAddingRegionalDirector, setIsAddingRegionalDirector] = useState(false)
-
+    const [instruments, setInstruments] = useState([])
     const [countries, setCountries] = useState([])
+    const [selectedInstrument, setSelectedInstrument] = useState('')
+    const [quantity, setQuantity] = useState(0)
+    const [addedInstruments, setAddedInstruments] = useState({})
+    const [newInstrumentName, setNewInstrumentName] = useState('')
+
 
     const [form, setForm] = useState({
         id: "",
@@ -54,52 +60,94 @@ export const ProgramUpdateEditForm = ( {programUpdateId} ) => {
     });
 
     useEffect(() => {
+        let programUpdateResponse, programResponse, countryResponse, instrumentResponse, instrumentChangeResponse;
         const program_data = async () => {
-            const programUpdate = await fetch(
+            // fetching program update to get program id
+            try {
+                programUpdateResponse = await fetch(
                     `http://localhost:3001/program-updates/${programUpdateId}`
-                ).then((r) => r.json());
-            console.log(programUpdate)
-            const program_id = programUpdate.programId; 
-
-            if (!program_id) {
-                console.log("No programid found on program update");
+                ).then((r) => {
+                    if (!r.ok) throw new Error(`Program Update Error: ${r.status}`)
+                    return r.json()
+            });
+            } catch (error) {
+                console.error('Error fetching program update: ', error)
                 return;
             }
+            const program_id = programUpdateResponse.programId;
+            console.log(program_id)
 
+            // fetching programs, countries, instruments
+            try {
+                [programResponse, countryResponse, instrumentResponse, instrumentChangeResponse] = await Promise.all([
+                    fetch(`http://localhost:3001/program/${program_id}`).then((r) => {
+                    if (!r.ok) throw new Error(`Program Error: ${r.status}`);
+                    return r.json();
+                    }),
+                    fetch(`http://localhost:3001/country`).then((r) => {
+                    if (!r.ok) throw new Error(`Country Error: ${r.status}`);
+                    return r.json();
+                    }),
+                    fetch(`http://localhost:3001/instruments`).then((r) => {
+                    if (!r.ok) throw new Error(`Country Error: ${r.status}`);
+                    return r.json();
+                    }),
+                    fetch(
+                    `http://localhost:3001/instrument-changes/`
+                    ).then((r) => {
+                    if (!r.ok) throw new Error(`Instrument Change Error: ${r.status}`)
+                    return r.json();
+                    })
+                ]);
+            }
+            catch (error){
+                console.error('Error fetching countries/program/instruments/instrument_change: ', error)
+                return;
+            }
+            console.log(instrumentChangeResponse);
 
-            const [
-                programResult,
-                countryResult,
-                programUpdateResult,
-            ] = await Promise.all([
-                fetch(`http://localhost:3001/program/${program_id}`).then(r => r.json()),
-                fetch(`http://localhost:3001/country`).then(r => r.json()),
-            ]);
+            // need to get instrument changes with the update id
+            // maybe should create route for selecting by update_id?
+            const filteredInstrumentChange = instrumentChangeResponse.filter(
+                (row) => row.updateId === programUpdateId
+            );
+
+            console.log(filteredInstrumentChange)
+            const match = new Map(instrumentResponse.map((i) => [i.id, i.name]));
+            console.log(match)
+            const existingInstruments = {};
+
+            for (const row of filteredInstrumentChange) {
+                const name = match.get(row.instrumentId);
+                console.log(name)
+                // if there is a duplicate just add it on top otherwise make it
+                existingInstruments[name] = (existingInstruments[name] ?? 0) + row.amountChanged;
+            }
+            console.log(existingInstruments)
+            setAddedInstruments(existingInstruments);
 
             setForm({
-                id: programResult.id ?? "",
-                created_by: programResult.createdBy ?? "",
-                program_name: programResult.name ?? "",
-                date_created: programResult.dateCreated
-                ? programResult.dateCreated.slice(0, 10)
+                id: programResponse.id ?? "",
+                created_by: programResponse.createdBy ?? "",
+                program_name: programResponse.name ?? "",
+                date_created: programResponse.dateCreated
+                ? programResponse.dateCreated.slice(0, 10)
                 : "",
-                country: programResult.country ?? "",
-                title: programResult.title ?? "",
-                description: programResult.description ?? "",
-                primary_language: programResult.primaryLanguage ?? "",
+                country: programResponse.country ?? "",
+                title: programResponse.title ?? "",
+                description: programResponse.description ?? "",
+                primary_language: programResponse.primaryLanguage ?? "",
                 playlist_link: "",
-                partner_org: programResult.partnerOrg ?? "",
-                program_status: programResult.status ?? "",
-                launch_date: programResult.launchDate
-                ? programResult.launchDate.slice(0, 10)
+                partner_org: programResponse.partnerOrg ?? "",
+                program_status: programResponse.status ?? "",
+                launch_date: programResponse.launchDate
+                ? programResponse.launchDate.slice(0, 10)
                 : "",
-                note: programUpdateResult ?? "",
-                countries: countryResult ?? ""
+                note: programUpdateResponse.note ?? "",
             });
 
-            setCountries(countryResult);
-
-            console.log("meow");
+            setCountries(countryResponse);
+            setInstruments(instrumentResponse)
         };
         program_data();
     }, [programUpdateId]);
@@ -108,6 +156,58 @@ export const ProgramUpdateEditForm = ( {programUpdateId} ) => {
         const { name, value } = e.target;
         setForm((prev) => ({ ...prev, [name]: value }));
     };
+
+    const addInstrument = async () => {
+        const name = newInstrumentName.trim()
+        if (!newInstrumentName.trim() || quantity <= 0) {
+            return;
+        }
+        // might want to check if instrument alr exists before adding
+        try {
+            const postResponse = await fetch('http://localhost:3001/instruments', {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({name}),
+            });
+
+        // reftch for dropdown
+        if (!postResponse.ok) {
+            throw new Error(`POST instrument error: ${postResponse.status}`);
+            }
+
+            const getResponse = await fetch("http://localhost:3001/instruments");
+
+            if (!getResponse.ok) {
+            throw new Error(`GET instruments failed: ${getResponse.status}`);
+            }
+
+            const instrumentsData = await getResponse.json();
+            setInstruments(instrumentsData);
+
+            setNewInstrumentName("");
+            return name;
+        } catch (error) {
+            console.error('Error adding instrument:', error);
+        }
+    };
+
+    const handleAddInstrumentAndQuantity = async () => {
+        const instrumentName = selectedInstrument || newInstrumentName.trim();
+
+        if (newInstrumentName) {
+            const added = await addInstrument();
+        }
+        setAddedInstruments(prev => ({
+            ...prev,
+            [instrumentName] : quantity
+        }));
+        setSelectedInstrument("");
+        setNewInstrumentName("");
+        setQuantity(0);
+    }
+
 
     return (
         <VStack p={8} width='35%' borderWidth="1px" borderColor="lightblue">
@@ -147,7 +247,7 @@ export const ProgramUpdateEditForm = ( {programUpdateId} ) => {
             <FormControl>
                 <FormLabel>Launch Date</FormLabel>
                 <Input
-                    name="program_launch_date"
+                    name="launch_date"
                     type='date' 
                     placeholder='Select Date'
                     value = {form.launch_date}
@@ -189,20 +289,61 @@ export const ProgramUpdateEditForm = ( {programUpdateId} ) => {
                 {!isAddingInstrument && <Button onClick={() => setIsAddingInstrument(true)}>+ Add</Button>}
                 {isAddingInstrument && 
                 <HStack>
-                    <Input placeholder="Instrument" />
-                    <NumberInput>
+                    <Select
+                        name="instrument"
+                        placeholder='Select Instrument'
+                        onChange={(e) => {
+                            setSelectedInstrument(e.target.value);
+                            setNewInstrumentName("");
+                        }}
+                        value={selectedInstrument}
+                    >
+                    {instruments.map(instrument => (
+                        <option key={instrument.id} value={instrument.name}>
+                            {instrument.name}
+                        </option>
+                    ))}
+                    </Select>
+                    <Input
+                        placeholder="Or type a new instrument"
+                        value={newInstrumentName}
+                        onChange={(e) => {
+                            setNewInstrumentName(e.target.value);
+                            setSelectedInstrument("");
+                        }}
+                    />
+                    <NumberInput
+                        value={quantity}
+                        onChange={(value) => setQuantity(parseInt(value) || 0)}>
                         <NumberInputField/>
                         <NumberInputStepper>
                             <NumberIncrementStepper/>
                             <NumberDecrementStepper/>
                         </NumberInputStepper>
                     </NumberInput>
-                    <Button onClick={() => setIsAddingInstrument(false)}>
+                    <Button
+                        onClick={async () => {
+                            await handleAddInstrumentAndQuantity();
+                            setIsAddingInstrument(false);
+                        }}
+                    >
                     + Add
                     </Button>
                 </HStack>               
                 }
             </FormControl>
+            {
+                Object.keys(addedInstruments).length > 0 && (
+                    <HStack width="100%" flexWrap="wrap" spacing={2}>
+                        {Object.entries(addedInstruments).map(([name, quantity]) => (
+                            <Tag key={name} size="lg" bg="gray.200">
+                                <TagLabel>{name} - {quantity}</TagLabel>
+                                <TagCloseButton onClick={() => removeInstrument(name)} />
+                            </Tag>
+                        ))}
+                    </HStack>
+                )
+            }
             <FormControl>
                 <FormLabel>Languages</FormLabel>
                 {!isAddingLanguage && <Button onClick={() => setIsAddingLanguage(true)}>+ Add</Button>}
