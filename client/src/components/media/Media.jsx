e8d84fc14047c8f3e9d1230733ab78030ae94b4c
Showmen Talukder <talukdershowmen@gmail.com>
2026-01-29 16:24:23 -0800

    fix: add spinner
    - add spinner
    - add TODOs comments
    - reformat

--- client/src/components/media/Media.jsx
+++ client/src/components/media/Media.jsx
@@ -1,13 +1,25 @@
 import { useEffect, useState } from "react";
-import { Box, Button, Container, Heading, VStack } from "@chakra-ui/react";
-import { MediaGrid } from "./MediaGrid";
+
+import {
+  Box,
+  Button,
+  Center,
+  Container,
+  Heading,
+  Spinner,
+  VStack,
+} from "@chakra-ui/react";
 
 import { useBackendContext } from "@/contexts/hooks/useBackendContext";
 import { useParams } from "react-router-dom";
 
+import { MediaGrid } from "./MediaGrid";
+
 export const Media = () => {
+  // TODO: Get userId from auth context when auth flow is finalized
   const { userId } = useParams();
   const { backend } = useBackendContext();
+
   const [media, setMedia] = useState([]);
   const [programName, setProgramName] = useState("");
   const [isLoading, setIsLoading] = useState(true);
@@ -17,9 +29,8 @@ export const Media = () => {
       setIsLoading(true);
 
       try {
-        //response gets media[], programName
         const response = await backend.get(`/mediaChange/${userId}/media`);
-        
+
         const transformedMedia = response.data.media.map((media) => ({
           id: media.id,
           s3_key: media.s3Key,
@@ -27,37 +38,56 @@ export const Media = () => {
           file_type: media.fileType,
           is_thumbnail: media.isThumbnail,
         }));
-  
+
         setMedia(transformedMedia);
         setProgramName(response.data.programName);
-  
       } catch (error) {
         console.error("Error loading media data:", error);
       } finally {
         setIsLoading(false);
-
       }
     };
-  
+
     fetchData();
   }, [backend, userId]);
 
   if (isLoading) {
     return (
-      <></>
+      <Center h="100vh">
+        <Spinner
+          thickness="4px"
+          speed="0.65s"
+          emptyColor="gray.200"
+          color="blue.500"
+          size="xl"
+        />
+      </Center>
     );
   }
 
   return (
     <Box minH="100vh">
-      <Container maxW="container.xl" py={8}>
-        <Box borderRadius="lg" p={8}>
-          <VStack align="start" spacing={6} w="full">
-            <Heading size="xl" color="gray.800">
+      <Container
+        maxW="container.xl"
+        py={8}
+      >
+        <Box
+          borderRadius="lg"
+          p={8}
+        >
+          <VStack
+            align="start"
+            spacing={6}
+            w="full"
+          >
+            <Heading
+              size="xl"
+              color="gray.800"
+            >
               {programName} Media
             </Heading>
 
-            {/* [TODO: BUTTON FOR ADDING NEW MEDIA] */}
+            {/* TODO: Implement functionality for button */}
             <Button
               variant="outline"
               bg="white"
@@ -68,7 +98,10 @@ export const Media = () => {
               + New
             </Button>
 
-            <MediaGrid mediaItems={media} programName={programName} />
+            <MediaGrid
+              mediaItems={media}
+              programName={programName}
+            />
           </VStack>
         </Box>
       </Container>
