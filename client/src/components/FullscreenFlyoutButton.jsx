40a6884f1e696af9837c4dbfaf9646803e96dc7b
Shayan Halder <shayanhalder2389@gmail.com>
2026-02-19 12:45:59 -0800

    chore: add full screen button to AccountForm.jsx

--- client/src/components/FullscreenFlyoutButton.jsx
+++ client/src/components/FullscreenFlyoutButton.jsx
@@ -3,8 +3,8 @@ import { Button } from '@chakra-ui/react';
 export function FullscreenFlyoutButton(props) {
     const { isFullScreen, toggleFullScreen, marginLeft, marginTop, width, height } = props;
     return (
-        <Button onClick={toggleFullScreen} aria-label={isFullScreen ? 'Exit' : 'Expand'} variant="outline" size="sm" marginLeft={marginLeft} marginTop={marginTop} width={width} height={height} >
-            {isFullScreen ? 'Exit' : 'Expand'}
+        <Button onClick={toggleFullScreen} aria-label={isFullScreen ? 'Minimize' : 'Expand'} variant="outline" size="sm" marginLeft={marginLeft} marginTop={marginTop} width={width} height={height} >
+            {isFullScreen ? 'Minimize' : 'Expand'}
         </Button>
     );
 }
