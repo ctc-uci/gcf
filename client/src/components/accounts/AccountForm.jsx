17d6f63fb905f33e657dc53f60526cdaef9ddcfb
Gina <fluffylennys@gmail.com>
2026-02-19 14:09:40 -0800

    Removed create admin option from admin view

--- client/src/components/accounts/AccountForm.jsx
+++ client/src/components/accounts/AccountForm.jsx
@@ -297,7 +297,7 @@ export const AccountForm = ({ targetUser, isOpen, onClose, onSave }) => {
                                     onChange={handleChange}
                                     value={formData.role}
                                 >
-                                    {role === 'Admin' && <option value = "Admin">Admin</option>}
+                                    {role === 'Super Admin' && <option value = "Admin">Admin</option>}
                                     {role === 'Admin' && <option value = "Regional Director">Regional Director</option>}
                                     <option value = "Program Director">Program Director</option>
                                 </Select>
