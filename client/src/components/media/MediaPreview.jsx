31195584030e1464fd6b6c4fed5965723efa9f8b
Harrison Tran <harrisontran357@gmail.com>
2026-02-24 23:04:39 -0800

    video/image is previewed next to uploads

--- client/src/components/media/MediaPreview.jsx
+++ client/src/components/media/MediaPreview.jsx
@@ -2,25 +2,64 @@ import {
   FormControl,
   Input,
   VStack,
+  Flex,
+  Center,
+  Image,
+  Box
 } from "@chakra-ui/react";
 
-export function MediaPreview({ title, onTitleChange }) {
+import gcf_globe from "../../../public/gcf_globe.png";
+
+import { useMemo } from "react";
+
+export function MediaPreview({ file, title, onTitleChange }) {
+  const previewUrl = useMemo(() => {
+    return URL.createObjectURL(file);
+  }, [file]);
 
   return (
     <VStack
         align="stretch"
         mb={2}
       >
-        <FormControl>
-          <Input
-            border="2px solid"
-            borderRadius="md"
-            borderColor="gray.100"
-            value={title}
-            placeholder="Add Title"
-            onChange={(e) => onTitleChange(e.target.value)}
-          />
-        </FormControl>
+        <Flex>
+          <Box h={"2rem"}>
+            {file.type.startsWith("video") ? (
+              <video
+                src={previewUrl}
+                style={{ width: "100%", height: "100%", objectFit: "contain" }}
+              />
+            ) : (
+              <Image
+                src={previewUrl}
+                alt="Preview"
+                w="100%"
+                h="100%"
+                objectFit="contain"
+                fallback={
+                  <Center h="100%">
+                    <Image
+                      src={gcf_globe}
+                      alt="Loading..."
+                      w="40px"
+                    />
+                  </Center>
+                }
+              />
+            )}
+          </Box>
+          <FormControl>
+            <Input
+              h={"2rem"}
+              border="2px solid"
+              borderRadius="md"
+              borderColor="gray.100"
+              value={title}
+              placeholder="Add Title"
+              onChange={(e) => onTitleChange(e.target.value)}
+            />
+          </FormControl>
+        </Flex>
       </VStack>
   );
 }
