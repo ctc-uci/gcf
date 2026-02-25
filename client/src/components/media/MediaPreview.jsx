518da470dc6c40ea69c19b2b1586339c1e0a9e97
Angelina Wang <wangangeliina@gmail.com>
2026-02-25 01:12:23 -0800

    fixed upload to s3 and POST request to user picture column

--- client/src/components/media/MediaPreview.jsx
+++ client/src/components/media/MediaPreview.jsx
@@ -54,7 +54,7 @@ export function MediaPreview({ file, title, onTitleChange }) {
               border="2px solid"
               borderRadius="md"
               borderColor="gray.100"
-              value={title}
+              value={title ?? ""}
               placeholder="Add Title"
               onChange={(e) => onTitleChange(e.target.value)}
             />
