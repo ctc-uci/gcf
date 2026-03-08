8496036cba3f31c23d37355dde78f8ea9a3fd057
Lawrence Zhou <zhoulh@uci.edu>
2026-03-07 16:25:14 -0800

    feat: added instrument_id to playlist

--- server/db/schema/playlist.sql
+++ server/db/schema/playlist.sql
@@ -1,10 +1,13 @@
 CREATE TABLE playlist (
     program_id BIGINT NOT NULL,
+    instrument_id BIGINT NOT NULL,
     link TEXT NOT NULL,
     name VARCHAR(256) NOT NULL,
     CONSTRAINT fk_program_id 
         FOREIGN KEY (program_id) 
         REFERENCES program(id),
-    PRIMARY KEY (program_id, link)
+    CONSTRAINT fk_instrument_id
+        FOREIGN KEY (instrument_id)
+        REFERENCES instrument(id),
+    PRIMARY KEY (program_id, instrument_id, link)
 );
-
