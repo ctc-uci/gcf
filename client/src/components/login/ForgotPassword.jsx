369c78066c2323c9d1db567e45939db1ffe028ad
Harrison Tran <harrisontran357@gmail.com>
2026-03-08 23:22:51 -0700

    error toast

--- client/src/components/login/ForgotPassword.jsx
+++ client/src/components/login/ForgotPassword.jsx
@@ -14,6 +14,7 @@ import {
   Heading,
   Input,
   Stack,
+  useToast,
 } from '@chakra-ui/react';
 
 import { BackendContext } from '../../contexts/BackendContext';
@@ -23,14 +24,24 @@ export const ForgotPassword = () => {
   const { backend } = useContext(BackendContext);
   const [errorMessage, setErrorMessage] = useState('');
   const auth = getAuth();
+  const toast = useToast();
 
   const verifyEmail = async (email) => {
+    setErrorMessage('');
     try {
       await backend.post('/gcf-users/verify-email', { email });
       await sendPasswordResetEmail(auth, email);
       setSubmitted(true);
     } catch {
-      setErrorMessage('No account associated with this email');
+      setErrorMessage('Invalid Email');
+      toast({
+        title: 'Email not found!',
+        description: 'Please check your email address',
+        status: 'error',
+        duration: 5000,
+        isClosable: true,
+        position: 'bottom-right',
+      });
     }
   };
   return (
@@ -53,7 +64,7 @@ export const ForgotPassword = () => {
           }}
         >
           <Stack mt="28px" spacing={7}>
-            <FormControl w={'100%'}>
+            <FormControl w={'100%'} isInvalid={!!errorMessage}>
               <FormLabel fontSize="lg">Email</FormLabel>
               <Center>
                 <Input
@@ -67,6 +78,7 @@ export const ForgotPassword = () => {
                   borderRadius="6px"
                   border="1px"
                   borderColor="gray.200"
+                  onChange={() => setErrorMessage('')}
                 />
               </Center>
               <FormErrorMessage>{errorMessage}</FormErrorMessage>
@@ -81,6 +93,7 @@ export const ForgotPassword = () => {
                 borderRadius="6px"
                 w="full"
                 mt="18px"
+                _hover={{ bg: 'gray.800' }}
               >
                 <MdOutlineSubdirectoryArrowRight /> Submit
               </Button>
