a040239c61bd68bb08d15e2fd8773364fad8af12
Sophie Choi <103692454+sophieelissachoi@users.noreply.github.com>
2026-04-07 11:35:42 -0700

    feat: pdf rendering now works, but the image is not rendering right

--- client/src/components/dashboard/PDF.jsx
+++ client/src/components/dashboard/PDF.jsx
@@ -1,9 +1,54 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 
-import { Text } from '@chakra-ui/react';
+import { useAuthContext } from '@/contexts/hooks/useAuthContext';
+import { useBackendContext } from '@/contexts/hooks/useBackendContext';
+
+import { MediaGrid } from '../media/MediaGrid';
 
 const PDF = () => {
-  return <Text>PDF</Text>;
+  const { currentUser } = useAuthContext();
+  const userId = currentUser?.uid;
+  const { backend } = useBackendContext();
+  const [pdfs, setPdfs] = useState([]);
+  const [programName, setProgramName] = useState('');
+
+  useEffect(() => {
+    const fetchPdfs = async () => {
+      try {
+        const res = await backend.get(`/mediaChange/${userId}/pdf`);
+
+        const transformedMedia = await Promise.all(
+          res.data.media.map(async (media) => {
+            const urlResponse = await backend.get(
+              `/images/url/${encodeURIComponent(media.s3Key)}`
+            );
+            return {
+              id: media.id,
+              s3_key: media.s3Key,
+              file_name: media.fileName,
+              file_type: media.fileType,
+              is_thumbnail: media.isThumbnail,
+              imageUrl: urlResponse.data.url,
+            };
+          })
+        );
+
+        setPdfs(transformedMedia);
+        setProgramName(res.data.programName);
+      } catch (err) {
+        console.error('Error saving uploaded files:', err);
+      }
+    };
+
+    fetchPdfs();
+  }, [userId, backend]);
+
+  return (
+    <MediaGrid
+      mediaItems={pdfs}
+      programName={programName}
+    />
+  );
 };
 
 export default PDF;
