a907ec12b661a5c6e40677a513b5025b55bd2e9f
Shayan Halder <shayanhalder2389@gmail.com>
2026-04-08 18:30:37 -0700

    feat: add account_change.sql

---
+++ server/db/schema/account_change.sql
@@ -0,0 +1,26 @@
+
+CREATE TABLE IF NOT EXISTS public.account_change (
+    id BIGSERIAL PRIMARY KEY,
+    user_id TEXT NOT NULL,
+    author_id TEXT NOT NULL,
+    
+    change_type VARCHAR(50) NOT NULL CHECK (change_type IN ('Creation', 'Update', 'Deletion')),
+    old_values JSONB,
+    new_values JSONB,
+    resolved BOOLEAN NOT NULL DEFAULT FALSE,
+    last_modified TIMESTAMP NOT NULL,
+
+    CONSTRAINT fk_user_id
+        FOREIGN KEY (user_id)
+            REFERENCES public.gcf_user(id)
+            ON DELETE CASCADE,
+
+    CONSTRAINT fk_author_id
+        FOREIGN KEY (author_id)
+            REFERENCES public.gcf_user(id)
+            ON DELETE CASCADE
+    
+);
+
+
+
