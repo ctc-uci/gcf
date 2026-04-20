1ba057aa2b5aef1a4ac29b180de59192a83a2520
alexy-ok <alexanderyin5280@gmail.com>
2026-04-19 18:17:33 -0700

    merge

---
+++ client/src/components/updates/forms/account-update-drawer/RoleSpecificDetails.jsx
@@ -0,0 +1,54 @@
+import { DiffField } from './DiffField';
+
+export const RoleSpecificDetails = ({
+  fields,
+  changeType,
+  roleDiffers,
+  programOldDisplay,
+  regionOldDisplay,
+  bioOldDisplay,
+  newRoleNorm,
+  t,
+}) => {
+  if (newRoleNorm === 'Program Director') {
+    return (
+      <>
+        <DiffField
+          label={t('common.program')}
+          oldValue={programOldDisplay}
+          newValue={fields.newProg}
+          changeType={changeType}
+          hasChangeOverride={roleDiffers ? undefined : fields.progById}
+        />
+        <DiffField
+          label={t('common.biography')}
+          oldValue={bioOldDisplay}
+          newValue={fields.newBio}
+          changeType={changeType}
+        />
+      </>
+    );
+  }
+
+  if (newRoleNorm === 'Regional Director') {
+    return (
+      <>
+        <DiffField
+          label={t('common.region')}
+          oldValue={regionOldDisplay}
+          newValue={fields.newReg}
+          changeType={changeType}
+          hasChangeOverride={roleDiffers ? undefined : fields.regById}
+        />
+        <DiffField
+          label={t('common.biography')}
+          oldValue={bioOldDisplay}
+          newValue={fields.newBio}
+          changeType={changeType}
+        />
+      </>
+    );
+  }
+
+  return null;
+};
