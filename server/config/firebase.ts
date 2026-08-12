14b73e90e6a8af3070e95d81a846642d5e77df00
Grace Yan <xgraceyan@gmail.com>
2026-08-11 21:30:03 -0700

    Use firebase-adminsdk JSON in dev

--- server/config/firebase.ts
+++ server/config/firebase.ts
@@ -1,11 +1,17 @@
 import firebaseAdmin, { type ServiceAccount } from 'firebase-admin';
 
-const rawJson = process.env.FIREBASE_ADMIN_SDK_JSON;
-if (!rawJson) {
-  throw new Error('FIREBASE_ADMIN_SDK_JSON env var is not set');
-}
+import localServiceAccount from './firebase-adminsdk.json';
 
-const serviceAccount = JSON.parse(rawJson) as ServiceAccount;
+let serviceAccount: ServiceAccount;
+if (process.env.NODE_ENV === 'development') {
+  serviceAccount = localServiceAccount as ServiceAccount;
+} else {
+  const rawJson = process.env.FIREBASE_ADMIN_SDK_JSON;
+  if (!rawJson) {
+    throw new Error('FIREBASE_ADMIN_SDK_JSON env var is not set');
+  }
+  serviceAccount = JSON.parse(rawJson) as ServiceAccount;
+}
 
 export const admin = firebaseAdmin.initializeApp({
   credential: firebaseAdmin.credential.cert(serviceAccount),
